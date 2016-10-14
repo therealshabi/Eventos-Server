9ee87e96dc17b000091ae7f334534ad0400ff8e4
gurleensethi <sarusethi@rocketmail.com>
2016-10-14 13:30:57 +0530

    Update routes

--- controllers/routes.js
+++ controllers/routes.js
@@ -5,8 +5,13 @@ var helper = require('../config/helper');
 module.exports = function(server) {
     //[GET, POST, PUT, DELETE] routes defined here
 
-    //GET REQUEST TO RESTURN ALL EVENTS
-    server.get('/events', function(req, res, next){
-
+    //[GET] REQUEST TO RESTURN ALL EVENTS
+    server.get('/events', function(req, res, next) {
+        helper.success(res, next, "HELLO");
     });
+
+    //[POST] REQUEST TO ADD NEW EVENTS
+    server.post('/events', function(req, res, next) {
+        
+    })
 }
